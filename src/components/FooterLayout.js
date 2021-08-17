67e499045818112d2824eb0cc9e8a9aa4c570a33
onives <olivenives63@gmail.com>
2021-08-17 10:57:51 +0300

    new commits

--- src/components/FooterLayout.js
+++ src/components/FooterLayout.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import{ Nav, Container, Row, Col} from 'react-bootstrap';
@@ -7,13 +6,13 @@ import{ Nav, Container, Row, Col} from 'react-bootstrap';
 
 const FooterLayout = ()=>{
    
+    const dateNow = new Date().getFullYear()
     return(
         <>
         <div id="footer">
             <Container>
                 <Row className="justify-content-center text-center">
-                    <Col><p className="footer-p">&copy; Created with <FontAwesomeIcon icon={faHeart} size="0.9x"/> by Nives</p></Col>
-                    <Col><Nav.Link href="https://www.linkedin.com/in/olive-nives-nameere" target='_blank'><FontAwesomeIcon icon={faLinkedin} size="2x"/></Nav.Link></Col>  
+                    <Col md={12}><p className="footer-p">&copy; {dateNow} Created with <FontAwesomeIcon icon={faHeart} size="0.9x"/> by Nives</p></Col> 
                 </Row>  
             
             </Container>
