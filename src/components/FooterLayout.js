fa18db141095f4eae8f9a54fd14a3b37552cc959
onives <olivenives63@gmail.com>
2021-08-17 10:48:59 +0300

    new commits

--- src/components/FooterLayout.js
+++ src/components/FooterLayout.js
@@ -1,28 +1,23 @@
 import React from 'react';
-import { faTwitter, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
-import{Navbar, Nav, Container, Row, Col} from 'react-bootstrap';
+import{ Nav, Container, Row, Col} from 'react-bootstrap';
 
 
 const FooterLayout = ()=>{
    
     return(
         <>
-        <Navbar id="footer">
-        <Container>
-            <Navbar.Brand>
-                <p>&copy; Created with <FontAwesomeIcon icon={faHeart} size="0.9x"/> by Nives</p>
-            </Navbar.Brand>
-            <Nav className="ml-auto">
-                <Row className="justify-content-center">
-                    <Col><Nav.Link href="https://twitter.com/nives__olive" target='_blank'><FontAwesomeIcon icon={faTwitter} size="2x"/></Nav.Link></Col>
-                    <Col><Nav.Link href="https://www.linkedin.com/in/olive-nives-nameere" target='_blank'><FontAwesomeIcon icon={faLinkedin} size="2x"/></Nav.Link></Col>
-                        
+        <div id="footer">
+            <Container>
+                <Row className="justify-content-center text-center">
+                    <Col><p className="footer-p">&copy; Created with <FontAwesomeIcon icon={faHeart} size="0.9x"/> by Nives</p></Col>
+                    <Col><Nav.Link href="https://www.linkedin.com/in/olive-nives-nameere" target='_blank'><FontAwesomeIcon icon={faLinkedin} size="2x"/></Nav.Link></Col>  
                 </Row>  
-            </Nav>
-        </Container>
-        </Navbar>
+            
+            </Container>
+        </div>
         </>
     )
 }
